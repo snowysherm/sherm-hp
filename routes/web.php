97044685a99f6b1d7e8eb2395cf0330ab123680d
sherm <shermkaboom@gmail.com>
2024-01-31 12:43:08 +0100

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoreController;
-use App\Http\Controllers\MedalController;
+use App\Http\Controllers\HomeController;use App\Http\Controllers\LoreController;use App\Http\Controllers\MedalController;use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::prefix('/')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home.index');
@@ -31,3 +39,4 @@ Route::prefix('lore')->group(function () {
     Route::get('/', [LoreController::class, 'index'])->name('lore.index');
 });
 
+require __DIR__.'/auth.php';
